44fea637dc930f2974dd632bb4e0e6dd528ac2c8
Owner <ankitrajkaur@gmail.com>
2019-09-12 12:54:55 -0400

    tc2

--- tests/BlazeDemoTest.java
+++ tests/BlazeDemoTest.java
@@ -3,7 +3,9 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BlazeDemoTest {
@@ -22,7 +24,21 @@ public class BlazeDemoTest {
 		driver = new ChromeDriver();
 
 		driver.get("http://blazedemo.com/");
+		WebElement inputBox;
+		//to check number of cities
+		
+	WebElement selectButton = driver.findElement(
+				By.cssSelector("form-inline"));
 
 	}
+	@Test
+	public void test2() {
+		//select the fligt
+		WebElement selectFlight = driver.findElement(
+				By.cssSelector("btn btn-small"));
+		WebElement virginFligt = driver.findElement(
+				By.cssSelector("table+tr td"));
+	}
+	
 
 }
